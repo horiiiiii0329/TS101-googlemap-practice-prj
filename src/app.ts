1eea88a8d3018251699f9e080614ee05d6350c14
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-11-05 07:37:25 +0900

    initial setup

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,25 @@
+import axios from "axios";
+
 const form = document.querySelector("form")!;
 const addressinput = document.getElementById("address")! as HTMLInputElement;
-const YOUR_API_KEY="AIzaSyBxRWvc1F1zZYGHrc1yJ0Fw9jebBEbfe50"
+const YOUR_API_KEY = "AIzaSyBxRWvc1F1zZYGHrc1yJ0Fw9jebBEbfe50";
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddresss = addressinput.ariaValueMax;
-  
 
-  axios.get(
-`https://maps.googleapis.com/maps/api/geocode/json?address=${}&key=${YOUR_API_KEY}`,
-  );
+  axios
+    .get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddresss
+      )}&key=${YOUR_API_KEY}`
+    )
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 form.addEventListener("submit", searchAddressHandler);
