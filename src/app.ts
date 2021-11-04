2dd0b3660f4e1d42bf3192507411f27bed9f5f36
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-11-05 07:44:49 +0900

    done fetching by axios

--- src/app.ts
+++ src/app.ts
@@ -4,18 +4,26 @@ const form = document.querySelector("form")!;
 const addressinput = document.getElementById("address")! as HTMLInputElement;
 const YOUR_API_KEY = "AIzaSyBxRWvc1F1zZYGHrc1yJ0Fw9jebBEbfe50";
 
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } };
+  status: "OK" | "ZERP_RESULTS";
+};
+
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddresss = addressinput.ariaValueMax;
 
   axios
-    .get(
+    .get<GoogleGeocodingResponse>(
       `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
         enteredAddresss
       )}&key=${YOUR_API_KEY}`
     )
     .then((response) => {
-      console.log(response);
+      if (response.data.status !== "OK") {
+        throw new Error("COuld not fetch location");
+      }
+      const coordinates = response.data.results[0].geometry.location;
     })
     .catch((error) => {
       console.log(error);
